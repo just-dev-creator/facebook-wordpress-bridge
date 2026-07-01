be650b16de88b0b987b1c6926a78fb0e4d03715d
Justus <justus@justcoding.tech>
2026-07-01 15:17:32 +0200

    Add video support, different FB post formats

--- main.py
+++ main.py
@@ -32,6 +32,32 @@ def publish_wordpress_post(title, text, date: str, featured_image_id=None):
                       })
     print(f"Published post {title} with status code {r.status_code}")
 
+def upload_media_to_wordpress(media_url: str):
+    media_response = requests.get(media_url)
+    media_filename = media_url.split("/")[-1].split("?")[0]
+    media_upload_response = requests.post(f"{wordpress_base_url}wp-json/wp/v2/media",
+                                          headers={
+                                              "Content-Disposition": f'attachment; filename="{media_filename}"',
+                                              "Content-Type": media_response.headers["Content-Type"],
+                                              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3"
+                                          },
+                                          data=media_response.content,
+                                          auth=(wordpress_username, wordpress_password)
+                                          )
+    media_upload_response_json = media_upload_response.json()
+    if "id" in media_upload_response_json:
+        media_id = media_upload_response_json["id"]
+        media_url = media_upload_response_json["guid"]["rendered"]
+
+        return {
+            "id": media_id,
+            "url": media_url,
+            "filename": media_filename
+        }
+    else:
+        print(f"Failed to upload media: {media_upload_response.text}")
+        return None
+
 # Get page access token
 
 r = requests.get(f"{facebook_api_url}/me/accounts?access_token={facebook_auth_token}")
@@ -75,9 +101,20 @@ for post in posts:
 
     post_text: str = post["message"]
 
+    bpost_title = bpost_text = None
+
+    print(post_text)
+
     post_text_parts = post_text.split("+++")
-    bpost_title = post_text_parts[1]
-    bpost_text = post_text_parts[2].lstrip('\r\n')
+    if not len(post_text_parts) >= 3:
+        post_text_parts = post_text.split("\n")
+        print(post_text_parts)
+        if len(post_text_parts) >= 2:
+            bpost_title=post_text_parts[0]
+            bpost_text="\n".join(post_text_parts[1:]).lstrip('\r\n')
+    else:
+        bpost_title = post_text_parts[1]
+        bpost_text = post_text_parts[2].lstrip('\r\n')
 
     if not bpost_title or not bpost_text:
         with open("published_posts.txt", "a") as f:
@@ -93,36 +130,30 @@ for post in posts:
         if "subattachments" in attachments[0]:
             attachments = attachments[0]["subattachments"]["data"]
 
+        print(attachments)
+
         for attachment in attachments:
-            print("im here")
             if "media" in attachment:
                 media_url = attachment["media"]["image"]["src"]
+                if attachment["type"] == "video":
+                    video_url = attachment["media"]["source"]
 
-                # Download the image and upload it to WordPress
-                media_response = requests.get(media_url)
+                    video_upload = upload_media_to_wordpress(video_url)
+                    if not video_upload:
+                        print(f"Failed to upload video: {video_url}")
+                    else:
+                        bpost_text += f'\n\n<video controls style="max-width:100%; height:auto;"><source src="{video_upload["url"]}" type="video/mp4"></video>'
 
+                media_upload = upload_media_to_wordpress(media_url)
 
-                media_filename = media_url.split("/")[-1].split("?")[0]
+                if not media_upload:
+                    print(f"Failed to upload media: {media_url}")
+                    continue
 
-                media_upload_response = requests.post(f"{wordpress_base_url}wp-json/wp/v2/media",
-                    headers={
-                        "Content-Disposition": f'attachment; filename="{media_filename}"',
-                        "Content-Type": media_response.headers["Content-Type"],
-                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3"
-                    },
-                    data=media_response.content,
-                    auth=(wordpress_username, wordpress_password)
-                )
-                media_upload_response_json = media_upload_response.json()
+                if not bpost_featured_image_id:
+                    bpost_featured_image_id = media_upload["id"]
 
-                if "id" in media_upload_response_json:
-                    media_id = media_upload_response_json["id"]
-                    # Add WordPress media url to post text
-                    bpost_text += f'\n\n<img src="{media_upload_response_json["guid"]["rendered"]}" alt="{media_filename}" />'
-                    if not bpost_featured_image_id:
-                        bpost_featured_image_id = media_id
-                else:
-                    print(f"Failed to upload media: {media_upload_response.text}")
+                bpost_text += f'\n\n<img src="{media_upload["url"]}" alt="{media_upload["filename"]}" />'
 
     publish_wordpress_post(bpost_title, bpost_text, post_date_str, bpost_featured_image_id)
 
